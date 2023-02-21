c3a47794d25ae7c01649fb3d49829ad80741fa63
Hashim Abdulaziz <hashimzezo222@gmail.com>
2023-02-21 10:06:51 -0800

    4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * _isalpha - a function that checks if character is alphabet
@@ -7,6 +7,7 @@
  *
  * Return: 1 if int c is alphabet, 0 if otherwise
  */
+
 int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 65 && c <= 90))
