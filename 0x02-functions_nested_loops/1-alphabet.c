74f99f8d20bb23f9152be37f2aabe5a80055ecc1
Hashim Abdulaziz <hashimzezo222@gmail.com>
2023-02-21 09:03:53 -0800

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main: bla bla
+ *
+ * Return: Always 0 (success).
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+
+	_putchar('\n');
+
+	return (0);
+}
