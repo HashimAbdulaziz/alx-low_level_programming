dc3ec3ddd3902c8fc6c9077fb3a3e4fe655f1b73
Hashim Abdulaziz <hashimzezo222@gmail.com>
2023-02-21 10:34:45 -0800

    6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - functions that get absolute value of number
+ *
+ * @n: @ab: integer input
+ *
+ * Return: abolute value of the number
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (n * -1);
+}
