a901bf89dc77614d0f17996647e2c6cad0b91dda
Hashim Abdulaziz <hashimzezo222@gmail.com>
2023-02-21 08:38:06 -0800

    0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+/*
+ * Task: functions
+ *
+ * Auther: Hashim Abdulaziz
+ */
+#include<stdio.h>
+
+/**
+ * main - about functions
+ *
+ * Return: Always 0 (success).
+ */
+
+int main(void)
+{
+	char s[] = "_putchar";
+	int i;
+
+	for(i = 0; i < 8; i++)
+		putchar(s[i]);
+
+	putchar('\n');
+
+	return (0);
+}
