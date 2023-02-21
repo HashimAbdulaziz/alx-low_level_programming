b3c0d4f98f7b0638a242933cd683ea4d1b82b09c
Hashim Abdulaziz <hashimzezo222@gmail.com>
2023-02-21 09:36:06 -0800

    2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - function prints alphabets 10 times
+ *
+ * Return: nothing
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	for (i = 0; i < 10; i++)
+		print_alphabet();
+}
