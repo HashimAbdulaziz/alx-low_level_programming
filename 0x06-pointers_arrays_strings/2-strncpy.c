9ab1a7140c6eb9b73bda2aca685d5ca8b6b3cd35
Hashim Abdulaziz <hashimzezo222@gmail.com>
2023-03-01 15:58:33 -0800

    2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strncpy _ a function that copies a string
+ *
+ * @dest: first input string
+ * @src: second input string
+ * @n: number of copied char
+ *
+ * Return: string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *end = dest;
+	int i, j;
+
+	for (i = 0; src[i] != '\0'; i++)
+		;
+
+	for (j = 0; j < n; j++)
+	{
+		if (j > i)
+			*end = '\0';
+		else
+			*end = src[j];
+		end++;
+	}
+
+	return (dest);
+}
